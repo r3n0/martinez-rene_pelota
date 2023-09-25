89cc6d41391402a48fd8410dfa8be4d1836a77a0
René Martínez <hola@r3n0.com>
2023-09-25 14:20:55 -0500

    se cambió círculo por rectángulo

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+	rect(mouseX, mouseY, 20);
 }
