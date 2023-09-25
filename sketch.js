f36b0daeaffd76d6841766e6f76376cab1bcb276
René Martínez <hola@r3n0.com>
2023-09-25 16:51:55 -0500

    la pelota aparece y desaparece por los bordes de la pantalla

--- sketch.js
+++ sketch.js
@@ -1,11 +1,32 @@
+let posX;
+let posY;
+let diametro;
+let radio;
+let fondo;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	posX = windowWidth / 2;
+	posY = windowHeight / 2;
+	// frameRate(10);
+	diametro = 100;
+	radio = diametro / 2;
+	fondo = color(25, 100, 50);
 }
-
 function draw() {
-	rect(mouseX, mouseY, 20);
-	rect(mouseX + 1, mouseY, 20);
+	background(fondo);
 
-	circle(200, 200, 50);
-	pint();
+	posX -= 10;
+	if (posX < -radio) {
+		diametro = random(10, 100);
+		radio = diametro / 2;
+		fondo = color(255, random(100, 255), 50);
+		fondo.saturation(0.7);
+		posX = windowWidth + radio;
+	}
+
+	fill(0);
+	circle(posX, posY, diametro);
+
+	print(posX);
 }
