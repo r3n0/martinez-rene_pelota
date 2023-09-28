c8121eb2889d40673ff95cfd0676b7f38b380df2
René Martínez <hola@r3n0.com>
2023-09-28 14:40:10 -0500

    updates

--- sketch.js
+++ sketch.js
@@ -1,32 +1,30 @@
 let posX;
 let posY;
-let diametro;
-let radio;
-let fondo;
+let diametro = 100;
+let radio = diametro / 2;
+let colorDeFondo;
+let vel = 10;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	posX = windowWidth / 2;
 	posY = windowHeight / 2;
 	// frameRate(10);
-	diametro = 100;
-	radio = diametro / 2;
-	fondo = color(25, 100, 50);
+	colorDeFondo = color(255, 200, 50);
 }
-function draw() {
-	background(fondo);
 
-	posX -= 10;
+function draw() {
+	background(colorDeFondo);
+	posX -= vel;
+
 	if (posX < -radio) {
 		diametro = random(10, 100);
 		radio = diametro / 2;
-		fondo = color(255, random(100, 255), 50);
-		fondo.saturation(0.7);
+		colorDeFondo = color(255, random(100, 255), 50);
+		// colorDeFondo.saturation(0.7);
 		posX = windowWidth + radio;
 	}
-
 	fill(0);
 	circle(posX, posY, diametro);
-
 	print(posX);
 }
